427af8b9bc369c77ff678a607d0d0de53f6d728c
gLima02 <104657034+gLima02@users.noreply.github.com>
2023-09-11 22:28:49 -0300

    Add files via upload

---
+++ AulaArvore/src/ex/Exercicio2.java
@@ -0,0 +1,42 @@
+package ex;
+import arvores.AVLInt;
+import java.util.Scanner;
+
+
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		Scanner le = new Scanner(System.in);
+		AVLInt avl = new AVLInt();
+
+		int opcao, valor;
+		do {
+			System.out.println(" 0 - Sair");
+			System.out.println(" 1 - Inserir");
+			System.out.println(" 2 - Mostrar FB dos nos");
+			opcao = le.nextInt();
+
+			switch (opcao) {
+			case 0:
+				System.out.println(" Encerrado o programa");
+				break;
+			case 1:
+				System.out.print("Informe valor: ");
+				valor = le.nextInt();
+				avl.root = avl.inserirH(avl.root, valor);
+				break;
+			case 2:
+				System.out.println("Apresentacao FB dos nos");
+				avl.mostraFB(avl.root);
+				System.out.println();
+				break;
+			default:
+				System.out.println("Opcao Invalida");
+			}
+		} while (opcao != 0);
+		le.close();
+
+	}
+
+}
